d2b122a7c5cc2287230224c40da6b8b00333289d
Aldikitta <andi.kitta77@gmail.com>
2022-05-30 14:25:33 +0800

    Initial commit

---
+++ app/src/main/java/com/aldikitta/workmanagerexample/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.aldikitta.workmanagerexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.aldikitta.workmanagerexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            setOneTimeWorkRequest()
+        }
+    }
+
+    private fun setOneTimeWorkRequest() {
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .build()
+        WorkManager.getInstance(applicationContext)
+            .enqueue(uploadRequest)
+    }
+}
